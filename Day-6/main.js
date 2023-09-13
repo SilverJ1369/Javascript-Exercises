9dc38a218bf54fcccf45b81eff7fd1d0a46726fe
SilverJ1369 <35865016+SilverJ1369@users.noreply.github.com>
2023-09-12 19:26:01 -0500

    finished day 6 Exercises

--- Day-6/main.js
+++ Day-6/main.js
@@ -59,8 +59,70 @@ person3.age = 24;
 console.log(person3.age);
 
 //Exercise 6 (scary)
+//Done in seperate repo
 
 //Exercise 7
+class Library {
+    books = [];
+    addBook(book) {
+        this.books.push(book);
+    }
+    removeBook(bookTitle) {
+        for (let i = 0; i < this.books.length; i++) {
+            let book = this.books[i];
+            if (book.title == bookTitle) {
+              this.books.splice(i);
+            }
+    }
+}
+    listBooks() {
+        this.books.forEach((element) => console.log(element));
+    }
+}
+
+class Books {
+    #ISBN;
+    title;
+    author;
+    yearPublished;
+    genre;
+    constructor(ISBN, title, author, yearPublished, genre) {
+        this.#ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.genre = genre;
+    }
+}
+const myLibrary = new Library;
+myLibrary.addBook(new Books(1, "The Beginning", "That one guy", 1919, "Action"));
+myLibrary.addBook(new Books(2, "The Middle", "That one guy", 1921, "Action"));
+console.log(myLibrary);
+myLibrary.removeBook("The Middle");
+console.log(myLibrary);
+myLibrary.listBooks();
 
 //Exercise 8
+class Student {
+    name;
+    grades = [];
+    constructor(name, grades) {
+        this.name = name;
+        this.grades = grades || [];
+    }
+    addGrades(grade) {
+        this.grades.push(grade);
+    }
+    findAverage() {
+        return this.grades.reduce((sum, grade) => sum + grade, 0) / this.grades.length;
+    }
+}
 
+const myStudent = new Student("Joe");
+myStudent.addGrades(79);
+myStudent.addGrades(84);
+myStudent.addGrades(99);
+myStudent.addGrades(94);
+myStudent.addGrades(64);
+myStudent.addGrades(78);
+console.log(myStudent.findAverage());
