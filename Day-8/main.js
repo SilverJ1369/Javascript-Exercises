4932100d59d7e030078f81a7ed4452f7890b5219
SilverJ1369 <35865016+SilverJ1369@users.noreply.github.com>
2023-09-19 19:12:00 -0500

    finished with Exercises

--- Day-8/main.js
+++ Day-8/main.js
@@ -15,3 +15,30 @@ myButton.addEventListener('click', clickedAlert);
 function clickedAlert() {
     alert("Button was Clicked!");
 }
+
+//Exercise 5
+const inputText = document.querySelector('#textInput');
+const outputText = document.querySelector('#textDisplay');
+
+
+inputText.addEventListener('input', recordInput);
+
+function recordInput() {
+    outputText.innerText = inputText.value;
+}
+
+//Exercise 6
+const items = [
+    {name: 'Item 1'},
+    {name: 'Item 2'},
+    {name: 'Item 3'},
+    {name: 'Item 4'}
+];
+
+const bulletList = document.querySelector('#itemList');
+items.forEach((item) => {
+    let newLi = document.createElement("li");
+    newLi.innerText = item.name;
+    bulletList.appendChild(newLi);
+    bulletList.style.backgroundColor = "gray";
+})
